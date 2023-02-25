bd0bc5fc84a5f95cebc10104b7edcdc346cf4543
Md Arshad Ahmad <70535034+arshad831@users.noreply.github.com>
2023-02-25 11:53:10 +0400

    Update Chapter5-keys-Joins.sql

--- Chapter5-keys-Joins.sql
+++ Chapter5-keys-Joins.sql
@@ -1,8 +1,28 @@
-SELECT * FROM sakila.customer;
 
-SELECT * FROM sakila.address a
-WHERE a.district = 'Buenos Aires' ;
--- Find the first name, last name and address of a customers who reside in 'Buenos Aires' district
+#LEFT JOIN Sakila customers  and address id
+
+use sakila;
+select * from customer 
+
+
+
+select a.first_name,a.customer_id,a.address_id, b.district
+ from customer a
+left join address b
+on a.address_id = b.address_id;
+
+#LEFT JOIN Sakila customers ,store id and address id
+
+select a.first_name,a.customer_id,a.store_id,b.address_id,c.district
+ from customer as a
+left join store as b on
+a.store_id = b.store_id
+left join address as c on 
+b.address_id = c.address_id;
+
+
+
+
 
 use sakila;
 
@@ -24,16 +44,19 @@ SELECT * FROM sakila.actor;
 
 SELECT * FROM sakila.address;
 
-SELECT 'actor' as table_name , DATE(last_update) as last_updated_date FROM actor
+select * from address 
+where district = 'Buenos Aires' 
 UNION 
-SELECT 'address' , DATE(last_update) as last_updated_date  FROM address ;
+select * from address 
+where district = 'Alberta' ;
 
 select * from orders;
 
 # aggregate
 use classicmodels;
+# number of order per status
 
-SELECT     status , sum(*) as count
+SELECT     status , count(*) as count
 FROM     orders
 GROUP BY status;
 
@@ -43,29 +66,23 @@ GROUP BY status;
 SELECT 
     a.status,     SUM(b.quantityOrdered * b.priceEach) AS amount
 FROM    orders a
-INNER JOIN orderdetails b
+inner JOIN orderdetails b
     on a.orderNumber = b.orderNumber
 GROUP BY     status;
     
     
-     -- the total sales of the years after 2003 for shipped status of the order
+     -- the total sales of the years after 2003 for shipped status of the order 'using' with alias -- not recommend to start
 
-SELECT 
-    YEAR(orderDate) AS year,
-    SUM(quantityOrdered * priceEach) AS total
-FROM
-    orders
+SELECT     YEAR(orderDate) AS year,    SUM(quantityOrdered * priceEach) AS total
+FROM     orders
 INNER JOIN orderdetails 
     USING (orderNumber)
-WHERE
-    status = 'Shipped'
-GROUP BY 
-    year
-HAVING 
-    year > 2003;
+WHERE     status = 'Shipped'
+GROUP BY     year
+HAVING    year > 2003;
     
     
-    -- find customer Number, customerName,orderDate,requiredDate,shippedDate,checkNumber,paymentDate,amount where payment is recieved but item is not shipped
+  -- find customer Number, customerName,orderDate,requiredDate,shippedDate,checkNumber,paymentDate,amount where payment is recieved but item is not shipped
 
 SELECT c.customerNumber, c.customerName,o.orderDate,o.requiredDate,o.shippedDate,
 p.checkNumber,p.paymentDate,p.amount
