23cf0bcc97ce8db10a178dee08d30ead15ac5fc7
Md Arshad Ahmad <70535034+arshad831@users.noreply.github.com>
2023-03-04 12:41:59 +0400

    Update Chapter6-subqueryandcte.sql

--- Chapter6-subqueryandcte.sql
+++ Chapter6-subqueryandcte.sql
@@ -2,28 +2,44 @@
 #sub query
 
 
-Use hr;
+Use sakila;
 
-    
+SELECT MIN(salary) FROM employees;
+
+
+SELECT 	first_name, salary, department_id
+FROM 	employees
+WHERE 	salary = min(salary)
+              
+
     
 SELECT 	first_name, salary, department_id
 FROM 	employees
 WHERE 
-	salary = (SELECT MIN(salary)
+	salary = (SELECT min(salary)
 			  FROM employees);
-              
-              
-select salesRepEmployeeNumber, sum(amount) as salesrep_amount from (
-
-SELECT a.*,b.salesRepEmployeeNumber FROM classicmodels.payments a
+    
+    
+    #subquery - the salesrep who has done the more sales - performance analysis 
+    
+    
+    use classicmodels;
+    
+    SELECT a.*,b.salesRepEmployeeNumber FROM classicmodels.payments a
 left join classicmodels.customers b
 on a.customerNumber = b.customerNumber
 
 
+              
+select salesRepEmployeeNumber, sum(amount) as salesrep_amount from 
+(
+SELECT a.*,b.salesRepEmployeeNumber FROM classicmodels.payments a
+left join classicmodels.customers b
+on a.customerNumber = b.customerNumber
 ) a 
 group by salesRepEmployeeNumber 
+order by salesrep_amount desc
 ;
-
 
 #with cte
 
@@ -35,3 +51,4 @@ on a.customerNumber = b.customerNumber
 
 select salesRepEmployeeNumber, sum(amount) as salesrep_amount from cte
 group by salesRepEmployeeNumber 
+order by salesrep_amount desc;
